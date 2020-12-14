f618cb20a76a35c4245b4f397dd7973bfd06ad23
wanglongtechnologies <68694404+wanglongtechnologies@users.noreply.github.com>
2020-12-14 14:24:22 +0800

    Add files via upload

---
+++ global.js
@@ -0,0 +1,11 @@
+/** @format */
+
+firebase.initializeApp({
+  apiKey: 'AIzaSyA3KQkMV0Qd6daoII9VukwOZdFDUK-9EEo',
+  authDomain: 'mywellfair-v2.firebaseapp.com',
+  projectId: 'mywellfair-v2',
+});
+
+$ = function (query) {
+  return document.querySelector(query);
+};
---
+++ sign-in.js
@@ -0,0 +1,94 @@
+/** @format */
+
+function handleError(error) {
+  console.log(error);
+}
+
+$ = function (query) {
+  return document.querySelector(query);
+};
+
+firebase.auth().onAuthStateChanged(function (user) {
+  if (user) {
+    window.location.href = '../lobby/';
+  }
+});
+
+$('#btnSignInWithGoogle').addEventListener('click', function () {
+  this.disabled = true;
+
+  let provider = new firebase.auth.GoogleAuthProvider();
+  firebase
+    .auth()
+    .signInWithPopup(provider)
+    .then(function (result) {})
+    .catch(function (error) {
+      handleError(error);
+    });
+
+  this.disabled = false;
+});
+
+/*
+$('#btnSignInWithFacebook').addEventListener('click', function () {
+  this.disabled = true;
+
+  let provider = new firebase.auth.FacebookAuthProvider();
+  provider.addScope('email');
+  firebase
+    .auth()
+    .signInWithPopup(provider)
+    .then(function (result) {})
+    .catch(function (error) {
+      handleError(error);
+    });
+
+  this.disabled = false;
+});
+*/
+
+$('#btnSignInWithEmailLink').addEventListener('click', function () {
+  this.disabled = true;
+
+  let email = document.getElementById('email').value;
+  firebase
+    .auth()
+    .sendSignInLinkToEmail(email, {
+      url: window.location.href,
+      handleCodeInApp: true,
+      dynamicLinkDomain: 'mywellfair.page.link',
+    })
+    .then(function () {
+      window.localStorage.setItem('emailForSignIn', email);
+      Swal.fire('Check your inbox', 'The sign in link has been sent to your email. Click on the link to sign in!', 'success');
+      document.getElementById('email').value = '';
+    })
+    .catch(function (error) {
+      handleError(error);
+    });
+
+  this.disabled = false;
+});
+
+if (firebase.auth().isSignInWithEmailLink(window.location.href)) {
+  let email = window.localStorage.getItem('emailForSignIn');
+  if (email) {
+    firebase
+      .auth()
+      .setPersistence(firebase.auth.Auth.Persistence.LOCAL)
+      .then(function () {
+        return firebase
+          .auth()
+          .signInWithEmailLink(email, window.location.href)
+          .then(function (result) {
+            window.localStorage.removeItem('emailForSignIn');
+          })
+          .catch(function (error) {
+            handleError(error);
+          });
+      })
+      .catch(function (error) {
+        handleError(error);
+      });
+  }
+}
